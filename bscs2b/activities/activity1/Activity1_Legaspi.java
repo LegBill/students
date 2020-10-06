4dc8d2fe9c3a56b84685f1a7fc95f978a425d1b2
LegBill <72184803+LegBill@users.noreply.github.com>
2020-10-06 23:06:04 +0800

    Add files via upload

---
+++ bscs2b/activities/activity1/Activity1_Legaspi.java
@@ -0,0 +1,14 @@
+public class Activity1_Legaspi
+{
+	public static void main(String[] args) {
+	    
+	    int Bill1=15;
+	    int Bill2=10;
+	    
+		System.out.println("The sum of 9 and 10 is "+(Bill1+Bill2));
+		System.out.println("The difference of 9 and 10 is "+(Bill1-Bill2));
+		System.out.println("The product of 9 and 10 is "+(Bill1*Bill2));
+		System.out.println("The quotient of 9 and 10 is "+Bill1/Bill2+"."+Bill1%Bill2);
+		System.out.println("Hello"+"World!!!");
+	}
+}
